2229de141de02358fb608507636042656bed044d
Tsutomu Yamaguchi <t.yamaguchi.air@outlook.jp>
2018-10-02 12:07:28 +0900

    Initial Commit

---
+++ NeuralNet1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NeuralNet1
+//
+//  Created by 山口勉 on 2018/10/02.
+//  Copyright © 2018年 山口勉. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
