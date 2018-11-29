7c454b674d20a0ec61b13c08d8aaf5022982b2f4
Tsutomu Yamaguchi <t.yamaguchi.air@outlook.jp>
2018-11-29 22:29:23 +0900

    test

---
+++ NeuralNet1/SimultaneousEqu.cpp
@@ -0,0 +1,9 @@
+//
+//  SimultaneousEqu.cpp
+//  NeuralNet1
+//
+//  Created by 山口勉 on 2018/11/15.
+//  Copyright © 2018年 山口勉. All rights reserved.
+//
+
+#include "SimultaneousEqu.hpp"
---
+++ NeuralNet1/SimultaneousEqu.hpp
@@ -0,0 +1,14 @@
+//
+//  SimultaneousEqu.hpp
+//  NeuralNet1
+//
+//  Created by 山口勉 on 2018/11/15.
+//  Copyright © 2018年 山口勉. All rights reserved.
+//
+
+#ifndef SimultaneousEqu_hpp
+#define SimultaneousEqu_hpp
+
+#include <stdio.h>
+
+#endif /* SimultaneousEqu_hpp */
